be62fe678150302476d2cd766a5e403d3f2470cf
siberiwolf@hotmail.com <Sitsang-1>
2017-06-19 19:48:18 +0800

    copy file

---
+++ io/src/main/java/com/gnu/CopyBytes.java
@@ -0,0 +1,35 @@
+package com.gnu;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+/**
+ * @author xuejiawei@lohogames.com on 2017-06-19
+ */
+public class CopyBytes {
+    public static void main(String[] args) throws Exception {
+        String dir = System.getProperty("user.dir");
+        System.out.println(String.format("current execute dictionary %s", dir));
+
+        FileInputStream in = null;
+        FileOutputStream out = null;
+
+        try {
+            in = new FileInputStream("hello.txt");
+            out = new FileOutputStream("copy.txt");
+            int c;
+
+            while ((c = in.read()) != -1) {
+                out.write(c);
+            }
+        } finally {
+            if (in != null) {
+                in.close();
+            }
+
+            if (out != null) {
+                out.close();
+            }
+        }
+    }
+}
