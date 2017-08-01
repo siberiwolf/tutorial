e28c5cd63cf09e4f1ddeb4fe5a7e7bfb5def893b
siberiwolf@hotmail.com <Sitsang-1>
2017-08-01 15:13:55 +0800

    append string

---
+++ string/src/main/java/com/gnu/string/AppendString.java
@@ -0,0 +1,20 @@
+package com.gnu.string;
+
+/**
+ * different append string
+ */
+public class AppendString
+{
+	public static void main(String[] args)
+	{
+		String str = 1 + 2 + "aaa";
+		// output 3aaa
+		System.out.println(str);
+
+		StringBuilder builder = new StringBuilder();
+		builder.append(1).append(2).append("aaa");
+		// output 12aaa
+		System.out.println(builder.toString());
+	}
+
+}
